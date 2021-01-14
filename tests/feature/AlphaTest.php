0427f6b1d448d31bebeb25ca3b01b5029f8d24da
Mahmoud Gabr <mahmoud.gabr@index.ae>
2021-01-14 09:18:22 +0400

    Test web page result

--- tests/feature/AlphaTest.php
+++ tests/feature/AlphaTest.php
@@ -1,3 +1,10 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
+final class AlphaTest extends TestCase
+{
+	public function testAlpha()
+	{
+		$this->get("/src/alpha.php");
+	}
+}
